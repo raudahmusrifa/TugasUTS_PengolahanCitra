24d60c1ab54fad82097be8236d43016b90f87563
raudahmusrifa <raudahmusrifa711@gmail.com>
2024-05-18 00:56:45 +0700

    uts

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Profile')
+st.subheader('Nama : RAUDAH MUSRIFA')
+st.subheader('NIM : 312210020')
+st.subheader('Kelas : TI.22.B1')
+st.subheader('Prodi : Teknik Infomatika')
+
+st.image("./img/Rraudah.png", caption="RAUDAH", use_column_width="Always", width=200)
+
+
+st.link_button("Instagram", "https://www.instagram.com/raudahfa/")
+st.link_button("Github", "https://github.com/raudahmusrifa")
