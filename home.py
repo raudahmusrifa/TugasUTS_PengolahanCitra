88812229fad2d3f7d6ac9dcc26d7f287ba9031b5
raudahmusrifa <115474431+raudahmusrifa@users.noreply.github.com>
2024-05-18 01:29:56 +0700

    Update home.py

--- home.py
+++ home.py
@@ -6,7 +6,7 @@ st.subheader('NIM : 312210020')
 st.subheader('Kelas : TI.22.B1')
 st.subheader('Prodi : Teknik Infomatika')
 
-st.image("./img/Rraudah.png", caption="RAUDAH", use_column_width="Always", width=200)
+st.image("./img/raudah.png", caption="RAUDAH", use_column_width="Always", width=200)
 
 
 st.link_button("Instagram", "https://www.instagram.com/raudahfa/")
